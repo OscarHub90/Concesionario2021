ead0a9d9a517a55c7449d1e447d7d55d7578d085
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-10-20 22:23:08 -0500

    Se agragan las rutas en Router, se presenta error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import Index from "pages/Index"
+import Admin from "pages/Admin";
+import Login from "pages/Login";
+import Registro from "pages/Registro";
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import "styles/styles.css";
 
 function App() {
-  return <div></div>
+  return (
+    <Router>
+      <Switch>
+        <Route to="/login">
+          <Login/>
+        </Route>
+        <Route to="/registro">
+          <Registro/>
+        </Route>
+        <Route to="/admin">
+          <Admin/>
+        </Route>
+        <Route to="/">
+          <Index />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
