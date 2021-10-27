5613336efdef698066902dffaf06e827bb29e6b3
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-10-26 23:11:53 -0500

    Se agrega estilos al Navbar y botón de inicial sesión

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="bg-blue-300">
+            <ul className="flex h-full justify-between my-4 "> 
+                <li>Logo</li>
+                <li>Logo</li>
+                <li>Navegación 1</li>
+                <li>Navegación 2</li>
+                <li>navegación 3</li>
+                <li className= "px-4">
+                    <button className=" bg-indigo-100 px-2 rounded-lg hover:bg-indigo-300">Iniciar Sesión</button>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Sidebar = () => {
-    return <nav className="w-72 bg bg-red-200">Sedebar</nav> /*w-72 es el ancho del sidebar */
+    return <nav className="w-72 bg bg-red-200">Sedebar</nav> /*w-72 es el ancho del sidebar*/
     
 }
 
