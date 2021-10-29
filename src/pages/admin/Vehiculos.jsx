7fb632eebe89edd148d14a5d193879fe637b0fe2
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-10-29 18:21:20 -0500

    Se agrega la primer función que escucha los cambios en el input descripción

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,12 +1,21 @@
 import React, {useEffect} from 'react'
 
 const Vehiculos = () => {
+    
+    useEffect(() => {
+    // Primer forma de usar el useEffect, con las variables vacías []
+    // cuándo alguna de las variables que están entre los corchetes cambien, se ejecutará la función en este caso solo la primera vez por estar vacías.
+    console.log("Hola, me ejecuto solo una vez por tener las variables vacías");
+    
+    }, []);
+
     return (
-    // se agrega la tabla para crear nuevos productos.
         <form className="flex flex-col">
-            <h2 className=" text-lg m-4">Formulario creación de vehículos</h2>
-        
-            <input type="text" placeholder= "Descripción" />
+            <h2 className=" text-lg m-4">Formulario creación de vehículos</h2> 
+            <input onChange = {(e) => {
+                console.log("Descripción: ",e.target.value)
+            }}
+            type="text" placeholder= "Descripción" />
             <input type="text" placeholder= "Marca"/>
             <input type="text" placeholder= "Modelo"/>
             <button className=" bg-blue-600 text-white rounded-lg  hover:bg-purple-800">Ingresar Datos</button>
