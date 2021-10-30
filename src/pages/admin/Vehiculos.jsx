49508e3ba56c777f05e503126f84f53d07428c9c
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-10-30 18:02:28 -0500

    Se agregan las funcionalidades al Botón de crear nuevos productos con renderización condicional

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,31 +1,35 @@
 import React, {useEffect, useState} from 'react'
 
 const Vehiculos = () => {
-    // para que el dato que el usuario ingrese se guarde en una variable: Definimos la variable marcaVehiculo, esto lee lo que tenga 
-    // la variable y luego le ponemos un set para que se puedan cambiar los datos de esa variable. por último definimos un estado con el que arranca la variable. 
-    const [marcaVehiculo, setmarcaVehiculo] = useState('') 
+    const [mostrarTabla, setmostrarTabla] = useState(true);
+    const [nombreBoton, setnombreBoton] = useState ("Crear Nuevo Producto");
 
-    useEffect(() => {
-    // Primer forma de usar el useEffect, con las variables vacías []
-    // cuándo alguna de las variables que están entre los corchetes cambien, se ejecutará la función en este caso solo la primera vez por estar vacías.
-    console.log("Hola, me ejecuto solo una vez por tener las variables vacías");
-    
-    }, []);
-
+    useEffect (() => {
+     if (mostrarTabla) {
+        setnombreBoton("Crear Nuevo Producto");
+    } else {
+        setnombreBoton("Mostrar Todos los productos");
+    }
+    },[mostrarTabla]); 
     return (
-        <form className="flex flex-col">
-            <h2 className=" text-lg m-4">Formulario creación de vehículos</h2> 
-            <input onChange = {(e) => {
-                console.log("Descripción: ",e.target.value)
-            }}
-            type="text" placeholder= "Descripción" />
-            <input onChange = {(e) => {
-                setmarcaVehiculo("Marca: ",e.target.value)
-            }}
-            type="text" placeholder= "Marca"/>
-            <input type="text" placeholder= "Modelo"/>
-            <button className=" bg-blue-600 text-white rounded-lg  hover:bg-purple-800">Ingresar Datos</button>
-        </form> 
-    )
+        <div>
+            <h2>ADMINISTRACIÓN DE PRODUCTOS</h2>
+            <button className="rounded-lg  bg-indigo-800 hover:bg-green-600 p-3 m-3 text-lg text-white" 
+            onClick = {() => {setmostrarTabla(!mostrarTabla)}} > {nombreBoton} </button>
+           
+            {mostrarTabla ? 
+            <TablaProdructos />
+            :<FormularioCreaciónProductos />}
+        </div>
+    );
 }
+
+const TablaProdructos = () => {
+    return <div>acá va la tabla de productos</div>
+};
+
+const FormularioCreaciónProductos = () => {
+    return <div>Acá va el formulario para crear productos</div>
+};
+
 export default Vehiculos
