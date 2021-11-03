cbd8649d9944e9fc0230202ea357662c6752226a
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-11-02 20:55:05 -0500

    Se reorganiza el formulario para crear nuevos productos, se crean las variables para recibir datos y se asigna el estado a cada input

--- src/pages/admin/Productos.jsx
+++ src/pages/admin/Productos.jsx
@@ -72,8 +72,8 @@ const TablaProdructos = ( { listaProductos } ) => {
         <table >
             <thead>
                 <tr>
-                    <th>Codigo</th>
-                    <th>Nombre</th>
+                    <th>ID De producto</th>
+                    <th>Descripción</th>
                     <th>Valor</th>
                     <th>Estado</th>
                 </tr>
@@ -96,14 +96,62 @@ const TablaProdructos = ( { listaProductos } ) => {
 };
 
 const FormularioCreaciónProductos = () => {
+
+    //Se crean las variables para almacenar la información de los productos
+    const [idProducto, setidProducto] = useState();
+    const [nombre, setnombre] = useState();
+    const [valor, setvalor] = useState();
+    const [estado, setestado] = useState();
+
     return (<div className="flex flex-col justify-center">
-            <h2 className="text-gray-900 font-extrabold">Formulario para productos nuevos</h2>
-            <form className= " grid grid-cols-1">
-                <input className="border-gray-700 bg-blue-100 m-2 p-2 rounded-xl" type="text" />
-                <input className="border-gray-700 bg-blue-100 m-2 p-2 rounded-xl" type="text" />
-                <input className="border-gray-700 bg-blue-100 m-2 p-2 rounded-xl" type="text" />
-                <input className="border-gray-700 bg-blue-100 m-2 p-2 rounded-xl" type="text" />
-                <button className="rounded-full  bg-green-500 hover:bg-green-700 p-3 m-3 text-lg text-white">Guardar Producto</button>
+            <h2 className="text-gray-900 font-extrabold m-8">Formulario para creación productos nuevos</h2>
+            <form className= " flex flex-auto grid-cols-1">
+                <label htmlFor="código"> ID De producto
+                    <input 
+                    name="código"
+                    className="border-gray-700 bg-blue-100 m-2 p-2 rounded-xl" type="text" placeholder="123456"
+                    
+                    value={idProducto}
+                    onChange={(e) => {
+                        setidProducto(e.target.value);
+                    }}             
+                    />
+                </label>
+                <label htmlFor="nombre"> Descripción
+                    <input 
+                    name="nombre"
+                    className="border-gray-700 bg-blue-100 m-2 p-2 rounded-xl" type="text"
+
+                    value={nombre}
+                    onChange={(e) => {
+                        setnombre(e.target.value);
+                    }}
+                    />
+                </label>
+                <label htmlFor="valor"> Valor Unitario
+                    <input 
+                    name="valor"
+                    className="border-gray-700 bg-blue-100 m-2 p-2 rounded-xl" type="text" placeholder="$"
+                    value={valor}
+                    onChange={(e) => {
+                        setvalor(e.target.value);
+                    }}
+                    />
+                </label>
+                <label htmlFor="estado"> Estado
+                    <select 
+                    value={estado}
+                    onChange={(e) => {
+                        setestado(e.target.value);
+                    }}
+                    className="border-gray-700 bg-blue-100 m-2 p-2 rounded-xl" type="text">
+                    <option disabled>Seleccione una opción</option>
+                        <option >Activo</option>
+                        <option >Inactivo</option>
+                    </select>
+                </label>
+
+                <button type='submit'className="rounded-full  bg-green-500 hover:bg-green-700 p-3 m-3 text-lg text-white">Guardar Producto</button>
             </form>
           </div>
     )
