16c43109fa2df3cd968a38926a94f2036ca23e3c
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-11-04 22:56:17 -0500

    Se agregan los botones para navegar desde el Admin

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
     return (
-    <nav className="w-72 h-full flex flex-col justify-center border border-gray-500 bg-purple-400">Sedebar
-    
-    </nav> /*w-72 es el ancho del sidebar*/
-    )
-}
+        <nav className='w-72 border border-gray-300 h-full flex flex-col bg-gray-200 p-4 sidebar'>
 
-export default Sidebar
+      <div className='my-4'>
+        <Ruta icono='fas fa-user' ruta='/admin/perfil' nombre='Perfil' />
+        <Ruta icono='fas fa-tshirt' ruta='/admin/productos' nombre='Productos' />
+        <Ruta icono='fas fa-wallet' ruta='/admin/ventas' nombre='Ventas' />
+        <Ruta icono='fas fa-users' ruta='/admin/usuarios' nombre='Usuarios' />
+      </div>
+      <button>Cerrar Sesión</button>
+    </nav>
+  );
+};
+
+const Ruta = ({ icono, ruta, nombre }) => {
+  return (
+    <Link to={ruta}>
+      <button className='p-1 my-2 bg-blue-900 hover:bg-green-900 hover:scale-75 flex w-full items-center text-white rounded-md'>
+        <i className={`${icono} w-10`} />
+        {nombre}
+      </button>
+    </Link>
+  );
+};
+
+export default Sidebar;
